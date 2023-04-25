a24578c6d3bcdba3be3fc55dc881902be85ba614
PhuongHN <hnphuong.ptit@gmail.com>
2023-04-25 22:45:39 +0700

    push develop

---
+++ ducnhung/src/main/java/wd/ducnhung/ducnhung/DucnhungApplication.java
@@ -0,0 +1,13 @@
+package wd.ducnhung.ducnhung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucnhungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DucnhungApplication.class, args);
+	}
+
+}
---
+++ ducnhung/src/test/java/wd/ducnhung/ducnhung/DucnhungApplicationTests.java
@@ -0,0 +1,13 @@
+package wd.ducnhung.ducnhung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DucnhungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
